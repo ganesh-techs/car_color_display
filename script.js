8802fc11b7f19fb13c9ed8cfd96f0112ad00eb0e
V-Code <pagairajasenthil@gmail.com>
2023-07-25 11:06:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+data=document.getElementById("im")
+data1=document.getElementById("cart")
+function blue() {
+     data.src='lambogini_blue.jpeg';
+  }
+  function green() {
+    data.src='lambogini_green.jpeg';
+ }
+ function white() {
+    data.src='lambogini_white.jpeg';
+ }
+ function add(){
+    data1.textContent="Added to cart"
+ }
+ 
+
+  
